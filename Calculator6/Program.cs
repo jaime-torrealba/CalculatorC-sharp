6166e9dd0c6065875c1bcf2ae684adb7d567e105
46766 <46766@DESKTOP-P0JDUG9>
2022-11-06 21:16:48 +0100

    Add project files.

---
+++ Calculator6/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Calculator6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my C#-calculator\r");
+            Console.WriteLine("...........................\n");
+            Console.Write("Enter a number and hit Enter: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter your second number and press Enter: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.ReadKey();
+
+            switch (Console.ReadLine()) { 
+            }
+
+            Console.WriteLine(num1 + num2);
+            Console.WriteLine(num1 - num2);
+            Console.WriteLine(num1 * num2);
+            Console.WriteLine(num1 / num2);
+
+            Console.Write("Press any key to close this window...");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
